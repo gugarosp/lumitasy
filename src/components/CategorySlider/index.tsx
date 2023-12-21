3eae6d383997919fe69dd98eab00742e8a063851
Gustavo Pereira <gugarosp@gmail.com>
2023-12-21 16:39:29 -0300

    Category Slider element: Context.Consumer changed for useContext

--- src/components/CategorySlider/index.tsx
+++ src/components/CategorySlider/index.tsx
@@ -3,6 +3,7 @@ import styles from "./CategorySlider.module.scss"
 import Poster from "elements/Poster";
 
 import { MoviesContext } from "context/movies";
+import { useContext } from "react";
 
 interface CategorySliderProps {
     categoryName: string;
@@ -13,28 +14,26 @@ interface MovieContextProps {
     moviesList: any[];
 }
 
+
 export default function CategorySlider({ categoryName, categorySlug }: CategorySliderProps) {
+    const { moviesList }:MovieContextProps = useContext(MoviesContext);
+
     return (
+        
+        <div className={`content ${styles["category-slider"]}`}>
+        <h3 className="no-margin">{categoryName}</h3>
 
-        <MoviesContext.Consumer>
-            {({moviesList}:MovieContextProps) => {
+        <div className={styles["category-slider-container"]}>
+            {
+                moviesList.filter(item => item.categories.includes(categorySlug)).map((item, index) => {
                 return (
-                    <div className={`content ${styles["category-slider"]}`}>
-                    <h3 className="no-margin">{categoryName}</h3>
+                    <Poster key={index} src={`movie/${item.slug}`} link="http://lumitasy.siteseguro.ws/images/movies/posters/poster-1.png" />
+                    )
+                })
+            }
+        </div>
 
-                    <div className={styles["category-slider-container"]}>
-                        {
-                            moviesList.filter(item => item.categories.includes(categorySlug)).map((item, index) => {
-                            return (
-                                <Poster key={index} src={`movie/${item.slug}`} link="http://lumitasy.siteseguro.ws/images/movies/posters/poster-1.png" />
-                                )
-                            })
-                        }
-                    </div>
+        </div>
 
-                    </div>
-                )
-            }}
-        </MoviesContext.Consumer>
     )
 }

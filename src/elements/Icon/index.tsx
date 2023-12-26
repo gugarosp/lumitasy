f147e5053d16c139badf4a12a7380b6033e14196
Gustavo Pereira <gugarosp@gmail.com>
2023-12-26 14:55:09 -0300

    Icon element created

---
+++ src/elements/Icon/index.tsx
@@ -0,0 +1,13 @@
+import { ReactElement } from "react"
+
+interface IconProps {
+    children: ReactElement | string
+}
+
+export default function Icon ({ children}: IconProps) {
+    return (
+        <span className="material-symbols-outlined">
+            {children}
+        </span>
+    )
+}

71faa03ab9a7b255b38ffafcc7a22e25289586e8
Gustavo Pereira <gugarosp@gmail.com>
2024-01-03 14:57:18 -0300

    Slider element: handle movement bug fixed (handle wasn't movinvg when mouse was outside it)

--- src/elements/Slider/index.tsx
+++ src/elements/Slider/index.tsx
@@ -3,59 +3,84 @@ import styles from "./Slider.module.scss";
 
 export default function Slider () {
 
+    // Receives the slider position
     const [currentSlidePosition, setCurrentSlidePosition] = useState<number>(0);
 
-    function sliderPosition (event:React.MouseEvent<HTMLDivElement, MouseEvent>) {
-        const sliderWidth = event.currentTarget.getBoundingClientRect().width;
-        const sliderBasePosition = event.currentTarget.getBoundingClientRect().x;
-        const sliderPosition = event.clientX;
-        const currentClick = sliderPosition - sliderBasePosition;
+    /////////
+    /* BAR */
+    /////////
 
-        setCurrentSlidePosition(Math.round(currentClick / sliderWidth * 100));
+    // Calculation for the new inner bar position when clicked in the background bar
+    function barClickPosition (event:React.MouseEvent<HTMLDivElement, MouseEvent>) {
+        const sliderWidth = event.currentTarget.getBoundingClientRect().width;
+        const sliderBaseXPosition = event.currentTarget.getBoundingClientRect().x;
+        const barClickPosition = event.clientX;
+        const newBarPosition = barClickPosition - sliderBaseXPosition;
+        const newBarPositionPercentage = newBarPosition / sliderWidth * 100
+
+        // Sets the new inner bar position
+        setCurrentSlidePosition(newBarPositionPercentage);
     }
 
+
+    /////////////
+    /* HANDLER */
+    /////////////
+
+    // Ref to get handler html element
     const handler = useRef<any>();
 
-    function slide (event:any) {
-        const sliderWidth = event.currentTarget.parentElement.querySelector(":first-child").getBoundingClientRect().width;
-        const sliderBasePosition = event.currentTarget.parentElement.querySelector(":first-child").getBoundingClientRect().x;
-        const dragPosition = event.clientX;
-        const handlePosition = dragPosition - sliderBasePosition;
-        let handlePositionPercentage = handlePosition / sliderWidth * 100;
+    // Calculation for the handle to slide according mouse movement
+    function slide (event:any):void {
+        const sliderWidth = handler.current.parentElement.querySelector(":first-child").getBoundingClientRect().width;
+        const sliderBaseXPosition = handler.current.parentElement.querySelector(":first-child").getBoundingClientRect().x;
+        const handlePosition = event.clientX;
+        const newHandlePosition = handlePosition - sliderBaseXPosition;
+        let newHandlePositionPercentage = newHandlePosition / sliderWidth * 100;
 
-        if (handlePositionPercentage < 0) {
-            handlePositionPercentage = 0;
-        } else if (handlePositionPercentage > 100) {
-            handlePositionPercentage = 100
+        if (newHandlePositionPercentage < 0) {
+            newHandlePositionPercentage = 0;
+        } else if (newHandlePositionPercentage > 100) {
+            newHandlePositionPercentage = 100
         }
-            
-        setCurrentSlidePosition(handlePositionPercentage);
+        
+        // Sets the new handle position
+        setCurrentSlidePosition(newHandlePositionPercentage);
 
         if (sliderMoving.current === false) {
-            return handler.current.removeEventListener("mousemove", slide);
+            return removeListeners();
         }
-
     }
 
-    const sliderMoving = useRef<boolean>(false)
-
-    function sliderHandle () {
-        sliderMoving.current = true;
-        handler.current.addEventListener("mousemove", slide);
+    // Remove added handle listeners
+    function removeListeners() {
+        window.removeEventListener("mousemove", slide);
+        window.removeEventListener("mouseup", finishSlideHandle);
     }
 
-    function removeSlideHandle() {
+    // Changes handle movement status so "function 'slide'" can remove added handle listeners 
+    function finishSlideHandle() {
         sliderMoving.current = false;
     }
 
+
+    // Starts handle movement/listeners
+    function startHandleMovement () {
+        sliderMoving.current = true;
+        window.addEventListener("mousemove", slide);
+        window.addEventListener("mouseup", finishSlideHandle);
+    }
+
+    // Handle movement status
+    const sliderMoving = useRef<boolean>(false)
+
     return (
         <div className={styles.slider}>
-            <div className={styles["background"]} onClick={event => sliderPosition(event)}>
+            <div className={styles["background"]} onClick={event => barClickPosition(event)}>
                 <div className={styles["current-position"]} style={{width: `${currentSlidePosition}%`}}></div>
             </div>
             <div ref={handler} className={styles.handler} style={{left: `${currentSlidePosition}%`}}
-                onMouseDown={sliderHandle}
-                onMouseUp={removeSlideHandle}
+                onMouseDown={startHandleMovement}
             ></div>
         </div>
     )

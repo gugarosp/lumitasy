9741318ddcbcb26adc7741df71f214b2b35cfef7
Gustavo Pereira <gugarosp@gmail.com>
2024-01-18 16:46:57 -0300

    Search page: search result developed

--- src/elements/Input/index.tsx
+++ src/elements/Input/index.tsx
@@ -1,13 +1,15 @@
 import styles from "./Input.module.scss"
 
 interface inputProps {
-    placeholder?: string 
+    placeholder?: string,
+    value?: string,
+    typing?: (event:React.ChangeEvent<HTMLInputElement>) => void
 }
 
-export default function Input ({placeholder = ""}:inputProps) {
+export default function Input ({placeholder = undefined, value = undefined, typing}:inputProps) {
     return (
         <div className={styles.input}>
-            <input type="text" placeholder={placeholder}/>
+            <input type="text" placeholder={placeholder} value={value} onChange={typing} />
         </div>
     )
 }
--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,7 +1,7 @@
 import styles from "./Search.module.scss"
 
 import Menu from "components/Menu";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { MoviesContext } from "context/movies";
 import Poster from "elements/Poster";
 import Input from "elements/Input";
@@ -9,40 +9,59 @@ import Input from "elements/Input";
 interface MovieContextProps {
     moviesList: {
         title: string,
-        slug:string,
+        slug: string,
         categories: string
         year: string
         description: string
     }[];
 }
 
-export default function Search () {
+export default function Search() {
 
-    const { moviesList }:MovieContextProps = useContext(MoviesContext);
+    // Movie list
+    const { moviesList }: MovieContextProps = useContext(MoviesContext);
+
+    // Search word
+    const [searchWord, setSearchWord] = useState("");
+
+    function typingEvent(event: React.ChangeEvent<HTMLInputElement>) {
+        setSearchWord(event.target.value);
+    }
+
+    // Search result
+    const searchResult = moviesList.filter(item => item.title.toLowerCase().includes(searchWord));
 
     return (
         <>
             <Menu />
             <section className={`${styles.search} outer-content-common`}>
-                <div className="content">
-                    <Input placeholder="Search"/>
+                <div className={`${styles.input} content`} key="search-input">
+                    <Input placeholder="Search" typing={(event) => typingEvent(event)} />
                 </div>
-                
-                <div className={`${styles.content} content`}>
-                    
+
+                <div className={`content`}>
                     {
-                        moviesList.map((item, index) => {
-                            return (
-                                <>
-                                    <Poster
-                                        key={index}
-                                        link={`movie/${item.slug}`}
-                                        src={`http://lumitasy.siteseguro.ws/images/movies/posters/${item.slug}.png`}
-                                        title={item.title}
-                                    />
-                                </>
-                            )
-                        })
+                        searchWord === "" ?
+                            <></>
+                        : searchResult.length !== 0 ?
+                            <div className={styles.result}>
+                                {
+                                    searchResult.map((item, index) => {
+                                        return (
+                                            <Poster
+                                                key={index}
+                                                link={`movie/${item.slug}`}
+                                                src={`http://lumitasy.siteseguro.ws/images/movies/posters/${item.slug}.png`}
+                                                title={item.title}
+                                            />
+                                        )
+                                    })
+                                }
+                            </div>
+                        : 
+                            <div className={styles["no-result"]}>
+                                No movie found
+                            </div>
                     }
                 </div>
             </section>

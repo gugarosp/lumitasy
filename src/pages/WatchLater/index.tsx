73d21e26ca89500fcae04d68612f93ce81449bed
Gustavo Pereira <gugarosp@gmail.com>
2024-02-02 16:54:35 -0300

    Watch Later page: Message added for no movies in the watch list

--- src/pages/WatchLater/index.tsx
+++ src/pages/WatchLater/index.tsx
@@ -3,12 +3,14 @@ import PosterList from "components/PosterList";
 import { useContext } from "react";
 import { MoviesContext } from "context/movies";
 import { IMoviesList, moviesContextType } from "context/moviesTypes";
+import SuperMessage from "components/SuperMessage";
 
 export default function WatchLater () {
 
     const { moviesList } = useContext(MoviesContext) as moviesContextType;
 
     const watchLaterIds = JSON.parse(localStorage.getItem("watch-later") || "[]");
+    console.log(watchLaterIds);
 
     const watchLaterList:IMoviesList[] = [];
     
@@ -21,12 +23,23 @@ export default function WatchLater () {
 
     return (
         <>
-        <Menu />
+            <Menu />
             <div className="outer-content-common">
                 <div className="content content-page">
                     <h1 className="no-margin">Watch Later</h1>
                     <div>
-                        <PosterList movieList={watchLaterList} />
+                        {
+                            watchLaterIds > 0 
+                            ?
+                            <PosterList movieList={watchLaterList} />
+                            :
+                            <SuperMessage
+                                icon="theaters"
+                                infoText="No Movies Added"
+                                messageTitle="You didn't add any movie to your Watch Later list"
+                                messageSubtitle={["It's easy! Find any movie you would like to watch and hit the Watch Later button"]}
+                            />
+                        }
                     </div>
                 </div>
             </div>

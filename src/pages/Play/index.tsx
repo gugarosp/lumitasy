e1d8d6f9e7648fe281772cbd72469a3d4f6b86eb
Gustavo Pereira <gugarosp@gmail.com>
2024-07-01 11:07:27 -0300

    Meta tags adjusted

--- src/pages/Play/index.tsx
+++ src/pages/Play/index.tsx
@@ -148,9 +148,9 @@ export default function Play () {
     // Page title
     document.title = movieInfo?.title !== undefined ? `${movieInfo.title} | Lumitasy` : "Lumitasy";
 
-        // Meta tags
-        document.querySelector("meta[name='title']")?.setAttribute("content", movieInfo?.title !== undefined ? `${movieInfo.title} | Lumitasy` : "Lumitasy");
-        document.querySelector("meta[name='description']")?.setAttribute("content", movieInfo?.description !== undefined ? `${movieInfo.description}` : "Public domain movie");
+    // Meta tags
+    document.querySelector("meta[name='title']")?.setAttribute("content", movieInfo?.title !== undefined ? `${movieInfo.title} | Lumitasy` : "Lumitasy");
+    document.querySelector("meta[name='description']")?.setAttribute("content", movieInfo?.description !== undefined ? `${movieInfo.description}` : "Public domain movie");
 
     return (
         <>  
